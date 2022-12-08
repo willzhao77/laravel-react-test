f92db0d91a2d8be8a7642545cf8f6118a37cf26e
Will Zhao <will@publica.co.nz>
2022-12-08 22:37:36 +1300

    add test route and component.

---
+++ resources/js/Pages/Test.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Test extends Component {
+    render() {
+        return (
+            <div>
+                Hello Laravel + React
+            </div>
+        );
+    }
+}
+
+export default Test;
